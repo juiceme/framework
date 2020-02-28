aa3f1ff4120668a6c64e8e47429ae078debab87d
Jussi Ohenoja <juice@swagman.org>
2020-02-28 13:24:57 +0200

    Add connection checking to pycli

--- examples/pycli.py
+++ examples/pycli.py
@@ -2,6 +2,8 @@ import requests
 import json
 import hashlib
 import pyaes
+import socket
+import sys
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -11,11 +13,22 @@ class ExampleConnection:
     """Encapsulates the connection to the example application"""
     def __init__(self, server, port):
         self.server = server
-        self.port = port
-        print("Connecting to " + self.server + ":" + self.port)
+        self.port = int(port)
+        self.token = ""
+        self.serial = ""
+        self.key = ""
+        print("Setting connection target " + self.server + ":" + str(self.port))
 
+    def check(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((self.server, self.port))
+        if result == 0:
+            return True
+        else:
+            return False
+    
     def do_login(self, username, password):
-        url = "http://" + self.server + ":" + self.port + "/api/login"
+        url = "http://" + self.server + ":" + str(self.port) + "/api/login"
         hashed_username = hashlib.sha1(username.encode('utf-8')).hexdigest()
         hashed_password = hashlib.sha1((password + hashed_username[0:4]).encode('utf-8')).hexdigest()
         data = { "username": hashed_username }
@@ -39,7 +52,7 @@ class ExampleConnection:
     def get_window(self, number):
         encrypted_data = pyaes.encrypt_message(self.key, json.dumps( { "serial": self.serial + 1,
                                                                        "token": self.token } ) )
-        url = "http://" + self.server + ":" + self.port + "/api/window/" + str(number)
+        url = "http://" + self.server + ":" + str(self.port) + "/api/window/" + str(number)
         data = { "token": self.token,
                  "data":  encrypted_data }
         res = requests.post(url, data=json.dumps(data))
@@ -47,15 +60,14 @@ class ExampleConnection:
             print("Error: cannot post message")
             return False
         else:
-            print("OK")
-            resp = json.loads(res._content.decode('utf-8'))
-            print(resp)
-            return True
+            return json.loads(res._content.decode('utf-8'))
 
-    
+
 #---------------
 
 connection = ExampleConnection("localhost", "8080")
+if connection.check() is False:
+    sys.exit("Cannot connect to the server")
 connection.do_login("test", "test")
-connection.get_window(1)
-
+window = connection.get_window(0)
+print(window["result"])
